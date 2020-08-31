ae3ddddbd028b7b908370a1c4be6768ab7ae7011
Veronica <veronica.21.coutts@gmail.com>
2020-08-31 14:10:01 +0200

    L4

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 class StorePicker extends React.Component {
     render() {
-        return <p>hello!</p>
+        return (
+            <Fragment>
+                { /** 
+                    Fragment allows for the wrapping of sibling elements, because 
+                    the return can only return one high level element.
+                    This is the same reason as to why you need to put the comment
+                    in here rather than above, as it will only return one element. 
+                */ }
+                <form className='store-selector'>
+                    <input type='text' required placeholder='Store Name' />
+                    <button type='submit'>Visit Store</button>
+                </form>
+                <p>hellooooo</p>
+            </Fragment>
+        );
     }
 }
 
